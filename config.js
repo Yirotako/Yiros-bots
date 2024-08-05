5e6a40bdbd8ce7ca2e79a22e8f7b68b302bb00dd
Yirotako <167583370+Yirotako@users.noreply.github.com>
2024-08-05 01:35:45 -0600

    config.js

--- config.js
+++ config.js
@@ -11,49 +11,49 @@ global.botnumber = ""
 global.confirmCode = ""
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
-  ['5212412377467', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215517489568', '💫 Mystic - Rey Endymion 💫', false],
-  ['59895555511', '💫 Mystic - Collaborator 8 💫', true],
-  ['573012482597', '💫 Mystic - OfcDiego', true],
-  ['59175655964', '💫 Mystic - By Alba070503 💫', true],
-  ['5493794297363', '💫 Mystic - Gabriel Ofc', true], 
-  ['393518398856', '💫 Mystic - Tester Fabri115💫', true],
-  ['212781160203', '💫 Mystic - Saad 💫', true], 
-  ['593968585283'],
-  ['5492266466080'],
-  ['5218442114446'],
-  ['59894808483'],
-  ['593980586516'], 
-  ['595975740803'],  
-  ['5492266613038'],
-  ['50497150165'],
-  ['51906662557'],
-  ['573183650526'], 
-  ['5217441298510'], 
-  ['5217294888993'],
-  ['595992611272'],
-  ['5219992843892'],
-  ['573147616444'], 
-  ['5219991402134'],
-  ['5219996125657'],
-  ['5492215034412'],
-  ['5492964650915']
+  ['5218134565961', '⚜️Teletransportation-bot ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot 2 ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot 3 ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot 4 ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot 5 ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot 6 ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot ⚜️', false],
+  ['5218134565961', '💫 Teletransportation-bot 8 ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot', true],
+  ['5218134565961', '💫 Teletransportation-bot ⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot⚜️', true], 
+  ['5218134565961', '💫 Teletransportation-bot⚜️', true],
+  ['5218134565961', '💫 Teletransportation-bot⚜️', true], 
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'], 
+  ['5218134565961'],  
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'], 
+  ['5218134565961'], 
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'], 
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961'],
+  ['5218134565961']
 ];
 
-global.suittag = ['5219993404349'];
-global.prems = ['51995386439'];
+global.suittag = ['5218134565961'];
+global.prems = ['5218134565961'];
 
-global.packname = 'Sticker';
-global.author = 'The Mystic - Bot';
-global.wm = 'The Mystic - Bot';
-global.titulowm = 'The Mystic - Bot';
-global.titulowm2 = `The Mystic - Bot`
-global.igfg = 'The Mystic - Bot';
+global.packname = 'Teletransportation-bot';
+global.author = 'Teletransportation-bot';
+global.wm = 'Teletransportation-bot';
+global.titulowm = 'Teletransportation-bot';
+global.titulowm2 = `Teletransportation-bot'`
+global.igfg = 'Teletransportation-bot';
 global.wait = '*_[ ⏳ ] Cargando..._*';
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg');
@@ -78,21 +78,21 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nThe Mystic - Bot`;
-global.gt = 'The Mystic - Bot';
-global.mysticbot = 'The Mystic - Bot';
+global.wm2 = `${dia} ${fecha}\nTeletransportation-bot`;
+global.gt = 'Teletransportation-bot';
+global.mysticbot = 'Teletransportation-bot';
 global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD';
 global.canal = 'https://whatsapp.com/channel/0029Vaein6eInlqIsCXpDs3y';
 global.waitt = '*_[ ⏳ ] Cargando..._*';
 global.waittt = '*_[ ⏳ ] Cargando..._*';
 global.waitttt = '*_[ ⏳ ] Cargando..._*';
-global.nomorown = '5219993404349';
+global.nomorown = '5218134565961';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
 global.cmenuf = '╰━═┅═━––––––๑\n';
-global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     ';
+global.cmenua = '\n⌕ ⚜️Teletransportation-bot⚜️ ⌕\n     ';
 global.dmenut = '*❖─┅──┅〈*';
 global.dmenub = '*┊»*';
 global.dmenub2 = '*┊*';
